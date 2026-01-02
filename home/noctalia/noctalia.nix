91848ce7421e3832b7bdfc42ade93606d476c522
BukoMoon <louisadams228@gmail.com>
2026-01-03 08:03:07 +1100

    Create noctalia.nix

---
+++ home/noctalia/noctalia.nix
@@ -0,0 +1,58 @@
+{ pkgs, ... }:
+
+{
+  programs.noctalia-shell = {
+    enable = true;
+
+    wallpaper = {
+      enabled = true;
+      overviewEnabled = true;
+      directory = "/home/buko/.config/Wallpapers";
+      enableMultiMonitorDirectories = true;
+      recursiveSearch = true;
+    };
+    colorSchemes = {
+      useWallpaperColors = true;
+      matugenSchemeType = "scheme-tonal-spot";
+      generateTemplatesForPredefined = true;
+    };
+    templates = {
+      gtk = true;
+      qt = true;
+      kcolorscheme = true;
+      cava = true;
+      kitty = true;
+      niri = true;
+      zed = true;
+      emacs = true;
+      enableUserTemplates = true;
+    };
+
+    dock = {
+      enabled = false;
+    };
+
+    general = {
+      avatarImage = "/home/buko/.face";
+      radiusRatio = 0.2;
+    };
+    ui = {
+      fontDefault = "CaskaydiaMono Nerd Font Mono";
+      fontFixed = "CaskaydiaMono Nerd Font Mono";
+      panelBackgroundOpacity = 0.8;
+    };
+    appLauncher = {
+      enableClipPreview = false;
+      showCategories = false;
+      terminalCommand = "kitty -e";
+      viewMode = "grid";
+    };
+    location = {
+      monthBeforeDay = true;
+      name = "Sydney, Australia";
+    };
+    audio = {
+      volumeOverdrive = true;
+    };
+  };
+}
