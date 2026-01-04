64178ad78d479af1b957e1d077e9f046adb5fc67
BukoMoon <louisadams228@gmail.com>
2026-01-04 17:48:24 +1100

    Added nvidia prime for hybrid graphics

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -21,5 +21,15 @@
     open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+    prime = {
+      sync.enable = true;
+
+      # Bus ID of the NVIDIA GPU. You can find it using lspci, either under 3D or VGA
+      nvidiaBusId = "PCI:1:0:0";
+
+      # Bus ID of the Intel GPU. You can find it using lspci, either under 3D or VGA
+      intelBusId = "PCI:0:2:0";
+    };
   };
 }
